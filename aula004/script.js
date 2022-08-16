850c7ed47b4d75044a699d0a73fa9736c114785f
dudaborges <duda.pborges92@gmail.com>
2022-08-16 18:51:05 -0300

    addJs

---
+++ aula004/script.js
@@ -0,0 +1,5 @@
+    // tudo o que for aparecer na tela terá que estar dentro de aspas
+    // não é obrigatório colocar ";" no final de cada código
+    window.alert('Minha primeira mensagem') //alerta com apenas uma opção "ok"
+    window.confirm('Está gostando de js?')  //confirmação com opção "ok" ou "cancelar"
+    window.prompt('Qual é seu nome?') //pergunta para preencher
