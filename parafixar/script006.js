b18edaafb26e5fae729e2b7c67b53c0da7bf63c5
dudaborges <duda.pborges92@gmail.com>
2022-08-16 20:31:31 -0300

    addJs

---
+++ parafixar/script006.js
@@ -0,0 +1,9 @@
+
+function somar(){
+    if (res >= 7){
+        sta.innerText(`A sua média deu ${res}, você está aprovado!`)
+    }else{
+        sta.innerText(`A sua média deu ${res}, você está reprovado!`)
+    }
+}
+
