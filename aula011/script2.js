d81f0ff5114a0e177c04d797b8f1d4765333ab2a
dudaborges <duda.pborges92@gmail.com>
2022-08-16 20:00:09 -0300

    addJs

---
+++ aula011/script2.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`Seu país é ${pais}`)
+//se por acaso o pais for igual a Brasil execute este comando
+if (pais == 'Brasil'){
+    console.log('Você é brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}
