d442d0efe36851feea0d5a597e02a5f0471c9d17
dudaborges <duda.pborges92@gmail.com>
2022-08-16 19:50:32 -0300

    addJs

---
+++ aula011/script.js
@@ -0,0 +1,13 @@
+//para executar: tecla f8
+
+ var vel = 65
+ console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+
+ if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+ }
+//lê-se: se por acaso a velocidade for maior que 60 execute este comando
+
+
+ console.log('Dirija sempre usando cinto de segurança!')
