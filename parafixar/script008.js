5bd3750ebb9b19dcd8ae81321e5421d0136cf7c2
dudaborges <duda.pborges92@gmail.com>
2022-08-17 17:42:37 -0300

    addJs

---
+++ parafixar/script008.js
@@ -0,0 +1,24 @@
+var n = 1
+
+if (n == 1){
+    console.log('Domingo')
+}
+if (n == 2){
+    console.log('Segunda-feira')
+}
+if (n == 3){
+    console.log('Terça-feira')
+}
+if (n == 4){
+    console.log('Quarta-feira')
+}
+if (n == 5){
+    console.log('Quinta-feira')
+}
+if (n == 6){
+    console.log('Sexta-feira')
+}
+if (n == 7){
+    console.log('Sábado')
+}
+
