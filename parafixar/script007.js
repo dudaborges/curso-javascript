ef8685afec6afbed8ca3ff106a07971e8ce52e21
dudaborges <duda.pborges92@gmail.com>
2022-08-17 17:33:51 -0300

    addJs

---
+++ parafixar/script007.js
@@ -0,0 +1,9 @@
+console.log('olá')
+
+var a = 'e'
+
+if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u'){
+    console.log('A letra digitada é uma vogal')
+}else{
+    console.log('A letra digitada é uma consoante')
+}
