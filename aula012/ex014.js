20a08f4779959c76c2ef1cc29e5f89b48a0635e7
dudaborges <duda.pborges92@gmail.com>
2022-08-17 19:46:58 -0300

    addJs

--- aula012/ex014.js
+++ aula012/ex014.js
@@ -1,9 +1,11 @@
 var idade = 14
 
-if (idade < 16){
-    console.log('Não vota')
-}else if (idade < 18){
-    console.log('Vota opcionalmente')
-}else if (idade >= 18){
+if (idade >= 18){
     console.log('Vota obrigatoriamente')
+}else {
+    if (idade < 16){
+        console.log('Não vota')
+    }else{
+        console.log('voto opcional')
+    }
 }
--- aula012/ex015.js
+++ aula012/ex015.js
@@ -1,9 +1,9 @@
 var idade = 15
-
+console.log(`Você tem ${idade} anos`)
 
 if (idade >= 18){
     console.log('Vota obrigatoriamente')
-}else if (idade >= 16){
+}else if (idade >= 16 || idade >= 65){
     console.log('Vota opcionalmente')
 }else if (idade < 16){
     console.log('Não vota')
