7619194e3108e1006b54b5703f85356306f4b985
dudaborges <duda.pborges92@gmail.com>
2022-08-17 11:49:18 -0300

    addJs

--- parafixar/script006.js
+++ parafixar/script006.js
@@ -1,9 +1,21 @@
-
 function somar(){
-    if (res >= 7){
-        sta.innerText(`A sua média deu ${res}, você está aprovado!`)
+    var tn1 = document.getElementById('txtn1')
+    var tn2 = document.getElementById('txtn2')
+    var res = document.getElementById('res')
+    
+    var n1 = Number(tn1.value)
+    var n2 = Number(tn2.value)
+    var t = (n1 + n2) / 2
+    
+    res.innerHTML = `A sua média deu ${t}`
+
+    if (t >= 7){
+        alert('Você está aprovado!')
     }else{
-        sta.innerText(`A sua média deu ${res}, você está reprovado!`)
+        alert('Você está reprovado!')
     }
+
 }
+
+
 
