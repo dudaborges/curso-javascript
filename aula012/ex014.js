7418cf943cb4232e3ca569e7b5023e16beee0799
dudaborges <duda.pborges92@gmail.com>
2022-08-17 19:34:24 -0300

    addJs

---
+++ aula012/ex014.js
@@ -0,0 +1,12 @@
+var idade = 17
+
+if(idade >= 18){
+    console.log('Vota obrigatoriamente')
+}else{
+    if(idade < 16){
+        console.log('Não vota')
+    }else{
+        console.log('Vota opcionalmente')
+    }
+
+}
