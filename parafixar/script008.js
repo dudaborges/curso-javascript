b72e3a8be662b8bd9b8d0f927dea599d42f1d954
dudaborges <duda.pborges92@gmail.com>
2022-08-18 19:12:58 -0300

    addJs

--- parafixar/script008.js
+++ parafixar/script008.js
@@ -1,24 +1,47 @@
-var n = 1
+//var hoje = new Date()
+//var n = hoje.getDay()
 
-if (n == 1){
+var n = 3
+
+/*if (n == 0){
     console.log('Domingo')
 }
-if (n == 2){
+if (n == 1){
     console.log('Segunda-feira')
 }
-if (n == 3){
+if (n == 2){
     console.log('Terça-feira')
 }
-if (n == 4){
+if (n == 3){
     console.log('Quarta-feira')
 }
-if (n == 5){
+if (n == 4){
     console.log('Quinta-feira')
 }
-if (n == 6){
+if (n == 5){
     console.log('Sexta-feira')
 }
-if (n == 7){
+if (n == 6){
     console.log('Sábado')
+}*/
+
+if (n == 0){
+    console.log('Domingo')
+}else if (n == 1){
+    console.log('Segunda-feira')
+}else if (n == 2){
+    console.log('Terça-feira')
+}else if (n == 3){
+    console.log('Quarta-feira')
+}else if (n == 4){
+    console.log('Quinta-feira')
+}else if (n == 5){
+    console.log('Sexta-feira')
+}else if (n == 6){
+    console.log('Sábado')
+}else{
+    console.log('[ERRO] Data inválida!')
 }
+
+
 
