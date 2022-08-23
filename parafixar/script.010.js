931431852f075558890db7abb6fbf1469e213681
dudaborges <duda.pborges92@gmail.com>
2022-08-23 18:12:23 -0300

    addJs

---
+++ parafixar/script.010.js
@@ -0,0 +1,9 @@
+var usuario = "aluno"
+
+if (usuario == "professor"){
+    console.log('Você logou como professor')
+}else if (usuario == "aluno"){
+    console.log('Você logou como aluno')
+}else{
+    console.log('Você logou como visitante')
+}
