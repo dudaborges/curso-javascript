37fc99af18a4ac7749d8d1aa1d37b72f6fb83732
dudaborges <duda.pborges92@gmail.com>
2022-08-28 22:39:20 -0300

    addJs

---
+++ parafixar/script010.js
@@ -0,0 +1,21 @@
+var voto = 15
+
+//13 - Lula, 22 - Bolsonaro, 12 - Ciro Gomes, 15 - Simone Tebet 
+
+switch (voto){
+    case 13:
+        console.log('Você votou no Lula')   
+        break 
+    case 22:
+        console.log('Você votou no Bolsonaro')
+        break
+    case 12:
+        console.log('Você votou no Ciro Gomes')    
+        break
+    case 15:
+        console.log('Você votou na Simone Tebet')
+        break
+    default:
+        console.log('[ERRO] voto inválido')    
+
+}
