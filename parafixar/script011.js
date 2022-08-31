2c8bf88279b7fba4fe2954d84ec49bdbd4d43dd7
dudaborges <duda.pborges92@gmail.com>
2022-08-31 20:17:48 -0300

    addJs

---
+++ parafixar/script011.js
@@ -0,0 +1,23 @@
+function mudarRosa(){
+    var rosa = document.getElementById('botaoRosa')
+
+    document.body.style.backgroundColor = "pink"
+    document.body.style.color = "black"
+
+
+}
+
+function mudarRoxo(){
+    var roxo = document.getElementById('botaoRoxo')
+
+    document.body.style.backgroundColor = "purple"
+    document.body.style.color = "black"
+}
+
+function mudarPreto(){
+    var preto = document.getElementById('botaoPreto')
+
+    document.body.style.backgroundColor = "black"
+
+    document.body.style.color ="white"
+}
