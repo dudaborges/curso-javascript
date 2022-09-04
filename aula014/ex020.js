709a714246f0b85d7c2fcf481d5ffa335560fdc5
dudaborges <duda.pborges92@gmail.com>
2022-09-04 11:27:33 -0300

    addJs

---
+++ aula014/ex020.js
@@ -0,0 +1,18 @@
+// var c = 1
+
+//  while (c <= 6) {
+//     console.log('Olá')
+//     c++ 
+//  }
+
+
+// do {
+//     console.log('Olá')
+//     c++ 
+//  } while (c <= 6)
+
+
+//inicialização | condição | incremento
+for (var c = 1; c <= 6; c++){
+
+}
