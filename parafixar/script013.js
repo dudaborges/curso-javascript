39fee021d97a00d1c01cdc63a14cbef7c5e9f293
dudaborges <duda.pborges92@gmail.com>
2022-09-07 20:31:40 -0300

    update

--- parafixar/script013.js
+++ parafixar/script013.js
@@ -1,7 +1,16 @@
 //mostrar tabuada de algum número
 
-for (num = 1; num <= 50; num++){
-    let numTab = 7
-    let res = numTab * num
-    console.log(`${numTab} x ${num} = ${res}`)
+// for (let num = 1; num <= 50; num++){
+//     let numTab = 7
+//     let res = numTab * num
+//     console.log(`${numTab} x ${num} = ${res}`)
+// }
+
+let nMulti = 1
+
+while(nMulti <= 10){
+    let nPrincipal = 2
+    let res = nPrincipal * nMulti
+    console.log(`${nPrincipal} x ${nMulti} = ${res}`)
+    nMulti++
 }
