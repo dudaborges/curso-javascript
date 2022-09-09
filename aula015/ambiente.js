20d92d337032a272d5bf32c255eac57ef282da74
dudaborges <duda.pborges92@gmail.com>
2022-09-08 21:06:18 -0300

    update

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,5 +1,6 @@
 let num = [5, 8, 2, 9, 3]
 num[3] = 6 //substitui um valor por outro pelo seu índice
 num[5] = 10 //adiciona o valor, já que foi colocado um índice não existente
+num.push(7) //acrescentar valores a uma array sem precisar saber sua posição
 
 console.log(`Nosso vetor é o ${num}`)
