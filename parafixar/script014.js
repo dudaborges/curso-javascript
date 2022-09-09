9626bc666d7b4b92a98f90eafc563d5599fcdcd0
dudaborges <duda.pborges92@gmail.com>
2022-09-09 10:35:12 -0300

    update

--- parafixar/script014.js
+++ parafixar/script014.js
@@ -1,5 +1,12 @@
 let frutas = ["banana", "uva", "mamão", "maça", "perâ"]
 frutas.push("morango")
 frutas[3] = "maçã"
+// console.log(frutas.length)
 
-console.log(frutas)
+
+for(let indice = 0; indice < frutas.length; indice++){
+    // console.log(`A fruta ${frutas[indice]} está na posição ${indice}`)
+    console.log(`${indice} - ${frutas[indice]}`)
+}
+
+
