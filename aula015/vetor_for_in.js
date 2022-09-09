35f0c71e93aeabc1864483c3b4d9336170388cd9
dudaborges <duda.pborges92@gmail.com>
2022-09-08 21:30:53 -0300

    update

--- aula015/vetor_for_in.js
+++ aula015/vetor_for_in.js
@@ -1,5 +1,5 @@
 let num = [5, 8, 2, 9, 3]
-
+console.log(num.indexOf(3)) //para encontrar o valor por meio de seu índice
 //lê-se: para cada posição dentro de num
 //for é para, in é dentro ou em
 for(let indice in num){
