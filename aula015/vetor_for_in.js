ed8163042488ff533a0daa591f5876605bade9a0
dudaborges <duda.pborges92@gmail.com>
2022-09-08 21:27:59 -0300

    update

---
+++ aula015/vetor_for_in.js
@@ -0,0 +1,7 @@
+let num = [5, 8, 2, 9, 3]
+
+//lê-se: para cada posição dentro de num
+//for é para, in é dentro ou em
+for(let indice in num){
+    console.log(`A posição ${indice} tem o valor ${num[indice]}`)
+}
