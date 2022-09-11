52ec281ec874933c5d397e32f6ebb70cb186a069
dudaborges <duda.pborges92@gmail.com>
2022-09-10 22:56:00 -0300

    update

--- parafixar/script016.js
+++ parafixar/script016.js
@@ -1,5 +1,7 @@
 let produtos = ["lápis", "borracha", "caneta"]
 
+console.log(`A lista contém ${produtos.length} ítens. São eles:`)
+
 for (let indice = 0; indice < produtos.length; indice++){
     console.log(`${indice} - ${produtos[indice]}`)
 }
