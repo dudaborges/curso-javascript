888ff200f8e88a3d40686e97d812a3503aaacfec
dudaborges <duda.pborges92@gmail.com>
2022-09-14 18:33:54 -0300

    added a sum function

---
+++ aula016/function_soma.js
@@ -0,0 +1,7 @@
+function soma (n1, n2){
+    return n1 + n2
+}
+//essa função recebe dois valores e retona a soma entre eles
+
+console.log(soma(5, 4))
+//imprime o retorno da função soma, substituindo os valores por 5 e 4
