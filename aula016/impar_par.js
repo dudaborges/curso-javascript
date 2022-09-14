9a39c886f70051c3e4e101a1aa7affb3a1a631cd
dudaborges <duda.pborges92@gmail.com>
2022-09-14 17:40:06 -0300

    function update

--- aula016/impar_par.js
+++ aula016/impar_par.js
@@ -1,8 +1,21 @@
-let num = 2
+// let num = 2
 
-//se o resto da divisão do número 2 for igual a zero
-if(num%2 == 0){
-    console.log('É par!')
-}else{
-    console.log('É ímpar!')
+// if(num%2 == 0){
+//     console.log('É par!')
+// }else{
+//     console.log('É ímpar!')
+// }
+
+//a função recebe um parâmetro n
+function num(n){
+    //se o resto da divisão do número 2 for igual a zero retorne 'par'
+    if(n%2 == 0){
+        return 'par'
+    }else{
+        return 'ímpar'
+    }
 }
+
+let res = num(11)
+
+console.log(res)
